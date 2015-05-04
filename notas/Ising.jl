c48e5103994a8535bab02c632ac69825361a62ee
Ernesto Paas <ernesto.paas@ciencias.unam.mx>
2015-05-04 14:52:11 -0500

    modulo Ising finalizado, empezar el  12

--- notas/Ising.jl
+++ notas/Ising.jl
@@ -1,7 +1,8 @@
 ##El modulo para trabajr con el modelo de Ising usando Metropolis-Hasting
-#module Ising
+module Ising
 
-#export configuracion, Configuracion, energia, energia!, δE, δE!, flip!, flip_matriz!
+export configuracion, Configuracion, configuracion, energia, energia!, δE, δE!, flip!, flip_matriz!, magnetizacion, magnetizacion!,
+        simulacion_con_E_y_M, simulacion_con_E_y_M_arreglo, promedio_propiedad, energia_magnetizacion_vs_temperatura
 
 ####  TIPOS  ####
 type Configuracion
@@ -15,7 +16,7 @@ type Configuracion
     posible_cambio::Array{Int64,1}
 end
 function configuracion(ancho::Int64,largo::Int64,temperatura::Float64)
-    regreso=Configuracion(ancho,largo,int(rand(ancho,largo))-1,1,0,temperatura,0,[0,0])
+    regreso=Configuracion(ancho,largo,int(rand(ancho,largo))-1,1.,0,temperatura,0,[0,0])
     regreso.magnetizacion=sum(regreso.matriz)
     regreso
 end
@@ -36,7 +37,7 @@ function magnetizacion!(conf::Configuracion)
 end
 
 #Calculo de Energia
-function energia(σ::Array{Int64,2},J::Float64=1)
+function energia(σ::Array{Int64,2},J::Float64=1.)
     L1=size(σ)[1]
     L2=size(σ)[2]
     E=0
@@ -53,15 +54,15 @@ function energia!(conf::Configuracion)
 end
 
 #Cambio de energia en un flip propuesto
-function δE(σ::Array{Int64,2},i::Int64=0,j::Int64=0,J::Float64=1)
+function δE(σ::Array{Int64,2},i::Int64=0,j::Int64=0,J::Float64=1.)
     if i==0 && j==0
         i,j=int((size(σ,1)-1)*rand())+1,int((size(σ,2)-1)*rand())+1
     end
     L1=size(σ)[1]
     L2=size(σ)[2]
 
-    2*σ[i,j]*(σ[modulo(i+1,L1),j]+σ[modulo(i-1,L1),j]+
-    σ[i,modulo(j+1,L2)]+σ[i,modulo(j-1,L2)]),
+    float( 2*σ[i,j]*(σ[modulo(i+1,L1),j]+σ[modulo(i-1,L1),j]+
+    σ[i,modulo(j+1,L2)]+σ[i,modulo(j-1,L2)]) ),
     i,j
 end
 function δE!(conf::Configuracion)
@@ -88,7 +89,7 @@ end
 function metropolis!(σ::Array{Int64,2},T::Float64)
     ΔE,i,j=δE(σ)
     ΔM=0
-    α=exp(-β(T)*ΔE)
+    α=exp(-1/T*ΔE)
     r=rand()
     cambio=false
     if r<α
@@ -102,9 +103,9 @@ function metropolis!(σ::Array{Int64,2},T::Float64)
 end
 
 ######Evolucion de una cadena#######
-function iteracion_con_E_y_M(espines::Array{Int64,2},iteraciones::Int64,T::Float64=1.0)
-    Es::Array{Int64}=Array(Int64, iteraciones)
-    Ms::Array{Int64}=Array(Int64, iteraciones)
+function simulacion_con_E_y_M_arreglo(espines::Array{Int64,2},iteraciones::Int64,T::Float64=1.0)
+    Es::Array{Float64}=Array(Float64, iteraciones)
+    Ms::Array{Float64}=Array(Float64, iteraciones)
     cambios::Array{Bool}=Array(Bool, iteraciones)
     E=energia(espines)
     M=magnetizacion(espines)
@@ -122,6 +123,11 @@ function iteracion_con_E_y_M(espines::Array{Int64,2},iteraciones::Int64,T::Float
 
     return Es,Ms,cambios
 end
+function simulacion_con_E_y_M(conf::Configuracion,iteraciones::Int64)
+    espines=conf.matriz
+    T=conf.temperatura
+    simulacion_con_E_y_M_arreglo(espines,iteraciones,T)
+end
 
 #Funcion para sacar el promedio de una propiedad que depende de la matriz de estabilidad de nuestro metod
 function promedio_propiedad(propiedad::Array{Float64,1},estados::Int64,funcion_tamano_sampleo::Function=sqrt)
@@ -134,14 +140,37 @@ function promedio_propiedad(propiedad::Array{Float64,1},estados::Int64,funcion_t
     sumProp/medidas
 end
 
-#Funcion que regresa un arreglo de la propieda promediada en el intervalo de temperaturas deseadas
-function propiedad_vs_temperatura(propiedad::Array{Float64,1},Tmin::Float64=0.1,Tmax::Float64=6.0,Tstep::Float64=0.3)
+#Funcion que regresa un arreglo de la propiedad promediada en el intervalo de temperaturas deseadas
+function energia_magnetizacion_vs_temperatura(conf::Configuracion,iteraciones::Int64,
+                                              Ts::Array{Float64,1}=[0.1:0.3:6.],funcion_tamano_sampleo::Function=sqrt)
+    #Unos contendores
+    ts_num=length(Ts)
+    E_proms=Array(Float64,ts_num)
+    M_proms=Array(Float64,ts_num)
+    estados=conf.ancho*conf.largo
 
+    #Sacamos el promedio
+    for i in 1:ts_num
+        conf.temperatura=Ts[i]
+        Es,Ms,cambios=simulacion_con_E_y_M(conf,iteraciones)
+        E_proms[i] = promedio_propiedad(Es,estados,funcion_tamano_sampleo)
+        M_proms[i] = promedio_propiedad(Ms,estados,funcion_tamano_sampleo)
+    end
+
+    return Ts,E_proms,M_proms
+end
+function propiedad_vs_beta(propiedad::Array{Float64,1},estados::Int64,funcion_tamano_sampleo::Function=sqrt,Ts::Array{Float64,1}=[0.1:0.3:6.])
+
+    f_proms=propiedad_vs_temperatura(propiedad,estados,funcion_tamano_sampleo,Ts)[2]
+
+    betas=similar(Ts)
+    for i in 1:length(Ts)
+        betas[i]=1/Ts[i]
+    end
+
+    return betas,f_proms
+end
+##Termina modulo
 end
 
-##Termina modulo
-#end
-
-###PRUEBAS
-using Ising
 
